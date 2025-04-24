1fa13baae3151f30e8d59988d122a27aa206b0f3
larads <dasilval.mariana@gmail.com>
2025-04-23 21:30:03 -0400

    feat: migrations in Drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,4 +3,11 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
     dialect: "postgresql",
     schema: "./src/db/schema/index.ts",
+    dbCredentials: {
+        host: 'localhost',
+        port: 5432,
+        user: 'docker',
+        password: 'docker',
+        database: 'pizza_shop'
+    }
 });
