45e5c47e23c2212ede439e0755f34fa77c8c31a6
larads <dasilval.mariana@gmail.com>
2025-04-26 22:53:27 -0400

    refactor: Update dependencies and environment setup in package.json and env.ts

---
+++ src/http/auth.ts
@@ -0,0 +1,35 @@
+import { Elysia, t, type Static } from 'elysia'
+import jwt from '@elysiajs/jwt'
+import cookie from '@elysiajs/cookie'
+
+import { env } from '../env'
+
+const jwtPayload = t.Object({
+    sub: t.String(),
+    restauranteId: t.Optional(t.String()),
+})
+
+export const auth = new Elysia()
+    .use(
+        jwt({
+            secret: env.JWT_SECRET_KEY,
+            schema: jwtPayload,
+        }),
+    )
+    .use(cookie())
+    .derive(({ jwt, setCookie, removeCookie }) => {
+        return {
+            signUser: async (payload: Static<typeof jwtPayload>) => {
+                const token = await jwt.sign(payload)
+
+                setCookie('auth', token, {
+                    httpOnly: true,
+                    maxAge: 60 * 60 * 24 * 7, // 7 days
+                    path: '/',
+                })
+            },
+            signOut: () => {
+                removeCookie('auth')
+            },
+        }
+    })
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,25 +1,14 @@
-import { Elysia, t } from 'elysia'
-import jwt from '@elysiajs/jwt'
-import cookie from '@elysiajs/cookie'
-
 import { registerRestaurante } from './routes/register-restaurante'
 import { sendAuthLink } from './routes/send-auth-link'
-
-import { env } from '../env'
+import { authenticateFromLink } from './routes/authenticate-from-link'
+import { signOut } from './routes/sign-out'
+import Elysia from 'elysia'
 
 const app = new Elysia()
-    .use(
-        jwt({
-            secret: env.JWT_SECRET_KEY,
-            schema: t.Object({
-                sub: t.String(),
-                restauranteId: t.Optional(t.String()),
-            }),
-        }),
-    )
-    .use(cookie())
     .use(registerRestaurante)
     .use(sendAuthLink)
+    .use(authenticateFromLink)
+    .use(signOut)
 
 app.listen(3333, () => {
     console.log('🔥 HTTP server running!')
